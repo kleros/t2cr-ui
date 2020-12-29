7ed8c7792e780b80624686a7fb11c7d5163fc7c4
mtsalenc <mtsalenc@gmail.com>
2020-12-29 19:14:49 -0300

    chore: project setup

---
+++ interfaces/index.ts
@@ -0,0 +1,10 @@
+// You can include shared interfaces/types in a separate file
+// And then use them in any component by importing them. For
+// Example, to import the interface below do:
+//
+// Import User from 'path/to/interfaces';
+
+export type User = {
+  id: number;
+  name: string;
+};
